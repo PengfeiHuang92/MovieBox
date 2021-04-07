ae4e362c4e628f438718b54f22f97c5d8bb73286
PengfeiHuang92 <12183278hpf@gmail.com>
2021-04-07 15:54:01 -0500

    master

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -2,10 +2,10 @@ import React from 'react'
 const SearchBox = ({ setQuery }) => {
   
     return (
-        <div >
-            <form className="SearchBox">
+        <div className="SearchBox">
+           
                 <input type="text" placeholder="Movie Name"  onChange={(e) => {setQuery(e.target.value)}} />
-            </form>
+     
         </div>
     )
 }
